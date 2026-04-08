4688e3b0c1ce47914bc80b54c8ba39840f9460ae
Hossam <hossamyasser1423@gmail.com>
2026-04-08 11:03:37 +0300

    ef core and using database migrations

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
